b5ceab8c1fa8e2fbeeb530f87fd076121dbb2d85
TONIXX <jiseok95@gmail.com>
2024-07-21 22:13:35 +0900

    Init: project

---
+++ src/main/java/com/tonixx/guesstitle/GuesstitleApplication.java
@@ -0,0 +1,13 @@
+package com.tonixx.guesstitle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesstitleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuesstitleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonixx/guesstitle/GuesstitleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonixx.guesstitle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesstitleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
